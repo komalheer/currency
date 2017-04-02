a70bd79e53aea4bbd831c3b41f5341639f34b98e
komalheer <komalheer1992@gmail.com>
2017-04-01 22:51:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const ls= localStorage;
+const ls= localStorage;
 var mydb=[];
 
  function show(){
 const base =document.getElementById("base").value;
 const con= document.getElementById("con").value;
+const convert= document.getElementById("convert").value;
 mydb.push(con);
+mydb.push(convert);
 ls.setItem('list',JSON.stringify(mydb))
 const show=JSON.parse(ls.getItem('list'))
 document.getElementById("con").innerHTML=show;
 console.log(show);
 const amount= document.getElementById("amount").value;
 
-fetch("https://api.fixer.io/latest?base="+base+"&symbols="+con)
+fetch("https://api.fixer.io/latest?base="+base+"&symbols="+con+convert)
   .then((res) => res.json())
   .then((data) =>{console.log(data.rates[con])
-	  var ans=amount*data.rates[con]+" "+ con 
-	  document.getElementById("display").innerHTML="The value is: "+ ans;
+	 
+	 
+	 
+	  var ans=amount*data.rates[con]+" "+ con ; 
+	  var ans1=amount*data.rates[convert]+" "+ convert 
+	  document.getElementById("display").innerHTML="Currency after conversion is: "+ ans;
+	  document.getElementById("display2").innerHTML="Currency after conversion is: "+ ans1;
  
   })
   .catch((e) => alert(`${e} There is some problem with your value`))
