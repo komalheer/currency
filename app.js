4a32de4f92a62c10133e52186479c868c5bf8d73
komal heer <komalheer1992@gmail.com>
2017-04-02 03:52:02 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const ls= localStorage;
+var mydb=[];
+
+ function show(){
+const base =document.getElementById("base").value;
+const con= document.getElementById("con").value;
+mydb.push(con);
+ls.setItem('list',JSON.stringify(mydb))
+const show=JSON.parse(ls.getItem('list'))
+document.getElementById("con").innerHTML=show;
+console.log(show);
+const amount= document.getElementById("amount").value;
+
+fetch("https://api.fixer.io/latest?base="+base+"&symbols="+con)
+  .then((res) => res.json())
+  .then((data) =>{console.log(data.rates[con])
+	 
+	 
+	 
+	  var ans=amount*data.rates[con]+" "+ con 
+	  document.getElementById("display").innerHTML="The value is: "+ ans;
+ 
+  })
+  .catch((e) => alert(`${e} There is some problem with your value`))
+  
+  }
+
+const btn = document.getElementById('click')
+btn.addEventListener('click', (e) => {
+	e.preventDefault()
+	show()
+})// JavaScript Document
